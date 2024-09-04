ba788c60d9337743df4ba99d8ab4a0fe2ddf50db
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-09-05 01:07:57 +0530

    chore: Delete agency and update document functionality

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -6,6 +6,7 @@ import { currentUser } from '@clerk/nextjs/server'
 import { Agency, IAgency } from '@/models/agency.model'
 import { get } from 'http'
 import { QueryCache } from 'react-query'
+import { UTApi } from 'uploadthing/server'
 // Write a query to save the current user's profile from Clerk provider in MongoDB
 export const initUser = async (newUser: Partial<IUser>) => {
   const user = await currentUser()
@@ -223,5 +224,113 @@ export const getAgency = async (id: string) => {
   } catch (error) {
     console.error('Error getting agency:', error)
     throw new Error('Error getting agency')
+  }
+}
+export const deleteAgency = async (id: string) => {
+  try {
+    await connectDB()
+    const agency = await Agency.findById({ id })
+    if (!agency) return 'Agency not found'
+    agency?.occupierDocuments?.photo &&
+      deleteFile({ fileKey: agency?.occupierDocuments?.photo })
+    agency?.occupierDocuments?.signature &&
+      deleteFile({ fileKey: agency?.occupierDocuments?.signature })
+    agency?.applicantIdProof?.electionId &&
+      deleteFile({ fileKey: agency?.applicantIdProof?.electionId })
+    agency?.applicantIdProof?.drivingLicense &&
+      deleteFile({ fileKey: agency?.applicantIdProof?.drivingLicense })
+    agency?.applicantIdProof?.aadharCard &&
+      deleteFile({ fileKey: agency?.applicantIdProof?.aadharCard })
+    agency?.applicantIdProof?.passport &&
+      deleteFile({ fileKey: agency?.applicantIdProof?.passport })
+    agency?.previousFactoryLicense?.previousFactoryLicense &&
+      deleteFile({
+        fileKey: agency?.previousFactoryLicense?.previousFactoryLicense,
+      })
+    agency?.previousFactoryLicense?.planApprovalLetter &&
+      deleteFile({
+        fileKey: agency?.previousFactoryLicense?.planApprovalLetter,
+      })
+    agency?.privateLimitedCompany?.moa &&
+      deleteFile({ fileKey: agency?.privateLimitedCompany?.moa })
+    agency?.privateLimitedCompany?.boardResolution &&
+      deleteFile({ fileKey: agency?.privateLimitedCompany?.boardResolution })
+    agency?.privateLimitedCompany?.form32 &&
+      deleteFile({ fileKey: agency?.privateLimitedCompany?.form32 })
+    agency?.listOfRawMaterials?.listOfRawMaterials &&
+      deleteFile({ fileKey: agency?.listOfRawMaterials?.listOfRawMaterials })
+    agency?.ownershipDocuments?.leaveAndLicenseAgreement &&
+      deleteFile({
+        fileKey: agency?.ownershipDocuments?.leaveAndLicenseAgreement,
+      })
+    agency?.ownershipDocuments?.midcAllotmentLetter &&
+      deleteFile({ fileKey: agency?.ownershipDocuments?.midcAllotmentLetter })
+    agency?.ownershipDocuments?.sevenTwelveExtract &&
+      deleteFile({ fileKey: agency?.ownershipDocuments?.sevenTwelveExtract })
+    agency?.ownershipDocuments?.taxReceipt &&
+      deleteFile({ fileKey: agency?.ownershipDocuments?.taxReceipt })
+    agency?.localAuthorityNoC?.localAuthorityNoC &&
+      deleteFile({ fileKey: agency?.localAuthorityNoC?.localAuthorityNoC })
+    agency?.localAuthorityNoC?.corporationNoC &&
+      deleteFile({ fileKey: agency?.localAuthorityNoC?.corporationNoC })
+    agency?.localAuthorityNoC?.grampanchayatNoC &&
+      deleteFile({ fileKey: agency?.localAuthorityNoC?.grampanchayatNoC })
+    agency?.localAuthorityNoC?.midcNoC &&
+      deleteFile({ fileKey: agency?.localAuthorityNoC?.midcNoC })
+    agency?.mpcbConsent?.mpcbConsent &&
+      deleteFile({ fileKey: agency?.mpcbConsent?.mpcbConsent })
+    agency?.sketchFactory?.sketch &&
+      deleteFile({ fileKey: agency?.sketchFactory?.sketch })
+    agency?.electricityBill?.electricityBill &&
+      deleteFile({ fileKey: agency?.electricityBill?.electricityBill })
+    agency?.electricityBill?.loadSanctionletter &&
+      deleteFile({ fileKey: agency?.electricityBill?.loadSanctionletter })
+    agency?.acceptanceLetter?.acceptanceLetter &&
+      deleteFile({ fileKey: agency?.acceptanceLetter?.acceptanceLetter })
+    agency?.flowChart?.flowChart &&
+      deleteFile({ fileKey: agency?.flowChart?.flowChart })
+    await Agency.findByIdAndDelete({
+      _id: id,
+    })
+
+    return agency
+  } catch (error) {
+    console.error('Error deleting agency:', error)
+    throw new Error('Error deleting agency')
+  }
+}
+export const updateDocument = async (
+  id: string,
+  documentPath: string,
+  newDocument: string
+) => {
+  try {
+    await connectDB()
+    const agency = await Agency.findOne({ id })
+    let doc = agency?.[documentPath]
+    await deleteFile(doc)
+    doc = newDocument
+    await Agency.findByIdAndUpdate(
+      agency?._id,
+      {
+        $set: {
+          [documentPath]: doc,
+        },
+      },
+      { new: true }
+    )
+  } catch (error) {
+    console.error('Error updating document:', error)
+    throw new Error('Error updating document')
+  }
+}
+
+const deleteFile = async ({ fileKey }: { fileKey: string | string[] }) => {
+  try {
+    const result = await UTApi.deleteFiles(fileKey)
+    return result
+  } catch (error) {
+    console.error('Error deleting file:', error)
+    throw new Error('Error deleting file')
   }
 }

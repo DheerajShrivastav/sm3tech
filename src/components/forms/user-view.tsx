fa7ce251365632817e0a9a49e1aeaed5ad7fd09a
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-09-05 01:07:47 +0530

    chore: Refactor UserView component to use Table component for displaying agency documents

--- src/components/forms/user-view.tsx
+++ src/components/forms/user-view.tsx
@@ -1,8 +1,17 @@
 'use client'
 import { getAgency } from '@/lib/queries'
 import { IAgency } from '@/models/agency.model'
-import { get } from 'http'
 import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '../ui/button'
 
 type Props = {
     id : string
@@ -19,94 +28,122 @@ const UserView = ({id}: Props) => {
     }, [id])
   return (
     <div>
-        <div className="max-w-md mx-auto p-4 bg-gray-100 shadow-md rounded">
-  <h2 className="text-lg font-bold mb-4">Agency Documents</h2>
-  <table className="w-full border-collapse">
-    <tbody>
-      <tr>
-        <th className="bg-gray-200 p-2">Document Type</th>
-        <th className="bg-gray-200 p-2">Document Name</th>
-        <th className="bg-gray-200 p-2">View</th>
-      </tr>
-      <tr>
-        <td className="p-2">Occupier Documents</td>
-        <td className="p-2">{agency?.occupierDocuments.name}</td>
-        <td className="p-2">
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            <a href={agency?.occupierDocuments.photo} target="_blank">
-              View Photo
-            </a>
-          </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            <a href={agency?.occupierDocuments.signature} target="_blank">
-              View Signature
-            </a>
-          </button>
-        </td>
-      </tr>
-      <tr>
-        <td className="p-2">Applicant ID Proof</td>
-        <td className="p-2">
-          {agency?.applicantIdProof.electionId ? (
-            <span>Election ID</span>
-          ) : (
-            <span>Driving License</span>
-          )}
-          {agency?.applicantIdProof.aadharCard ? (
-            <span>Aadhar Card</span>
-          ) : (
-            <span>Passport</span>
-          )}
-          {agency?.applicantIdProof.panCard ? (
-            <span>Pan Card</span>
-          ) : (
-            <span></span>
-          )}
-        </td>
-        <td className="p-2">
-          {agency?.applicantIdProof.electionId ? (
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              <a href={agency.applicantIdProof.electionId} target="_blank">
-                View Election ID
-              </a>
-            </button>
-          ) : (
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              <a href={agency?.applicantIdProof.drivingLicense} target="_blank">
-                View Driving License
-              </a>
-            </button>
-          )}
-          {agency?.applicantIdProof.aadharCard ? (
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              <a href={agency?.applicantIdProof.aadharCard} target="_blank">
-                View Aadhar Card
-              </a>
-            </button>
-          ) : (
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              <a href={agency?.applicantIdProof.passport} target="_blank">
-                View Passport
-              </a>
-            </button>
-          )}
-          {agency?.applicantIdProof.panCard ? (
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              <a href={agency?.applicantIdProof.panCard} target="_blank">
-                View Pan Card
-              </a>
-            </button>
-          ) : (
-            <span></span>
-          )}
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-
-
+      {/* <div className="max-w-md mx-auto p-4 bg-gray-100 shadow-md rounded"> */}
+        <div>
+        <h2 className="text-lg font-bold mb-4">Agency Documents</h2>
+        <Table className="w-full border-collapse">
+          <TableCaption>A list of your uploaded Documents</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead >Document Type</TableHead>
+              <TableHead >Document Name</TableHead>
+              <TableHead >View</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className=" font-medium">Agency Registration</TableCell>
+              <TableCell >
+                {agency?.occupierDocuments.name}
+              </TableCell>
+              <TableCell className="p-2">
+                <Button >
+                  <a href={agency?.occupierDocuments.photo} target="_blank">
+                    View Photo
+                  </a>
+                </Button>
+                <Button >
+                  <a href={agency?.occupierDocuments.signature} target="_blank">
+                    View Signature
+                  </a>
+                </Button>
+              </TableCell>
+            </TableRow>
+          <TableRow>
+              <TableCell className=" font-medium">Occupier Documents</TableCell>
+              <TableCell >{agency?.occupierDocuments.name}</TableCell>
+              <TableCell >
+                <Button >
+                  <a href={agency?.occupierDocuments.photo} target="_blank">
+                    View Photo
+                  </a>
+                </Button>
+                <Button >
+                  <a href={agency?.occupierDocuments.signature} target="_blank">
+                    View Signature
+                  </a>
+                </Button>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className=" font-medium">Applicant ID Proof</TableCell>
+              <TableCell >
+                {agency?.applicantIdProof.electionId ? (
+                  <span>Election ID</span>
+                ) : (
+                  <span>Driving License</span>
+                )}
+                {agency?.applicantIdProof.aadharCard ? (
+                  <span>Aadhar Card</span>
+                ) : (
+                  <span>Passport</span>
+                )}
+                {agency?.applicantIdProof.panCard ? (
+                  <span>Pan Card</span>
+                ) : (
+                  <span></span>
+                )}
+              </TableCell>
+              <TableCell >
+                {agency?.applicantIdProof.electionId ? (
+                  <Button >
+                    <a
+                      href={agency.applicantIdProof.electionId}
+                      target="_blank"
+                    >
+                      View Election ID
+                    </a>
+                  </Button>
+                ) : (
+                  <Button >
+                    <a
+                      href={agency?.applicantIdProof.drivingLicense}
+                      target="_blank"
+                    >
+                      View Driving License
+                    </a>
+                  </Button>
+                )}
+                {agency?.applicantIdProof.aadharCard ? (
+                  <Button >
+                    <a
+                      href={agency?.applicantIdProof.aadharCard}
+                      target="_blank"
+                    >
+                      View Aadhar Card
+                    </a>
+                  </Button>
+                ) : (
+                  <Button >
+                    <a href={agency?.applicantIdProof.passport} target="_blank">
+                      View Passport
+                    </a>
+                  </Button>
+                )}
+                {agency?.applicantIdProof.panCard ? (
+                  <Button >
+                    <a href={agency?.applicantIdProof.panCard} target="_blank">
+                      View Pan Card
+                    </a>
+                  </Button>
+                ) : (
+                  <span></span>
+                )}
+              </TableCell>
+            </TableRow> 
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }

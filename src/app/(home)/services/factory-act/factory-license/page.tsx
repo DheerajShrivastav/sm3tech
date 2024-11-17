0ce49e0ac632bc7d2d4d1a055405b4986d2b13df
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-11-18 00:16:27 +0530

    Add factory license page and user API route

---
+++ src/app/(home)/services/factory-act/factory-license/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import FactoryLicenseDetails from '@/components/forms/factorylicense-details'
+type Props = {}
+
+const factoryLicense = (props: Props) => {
+  return (
+    <div>
+      <FactoryLicenseDetails />
+    </div>
+  )
+}
+
+export default factoryLicense
